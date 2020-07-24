6e22a30e7d168504cf06ec2f55b0ff62c4d62dc9
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-07-24 11:26:06 -0700

    MergeSortIP

--- src/_00_Sorting_Algorithms/InsertionSorter.java
+++ src/_00_Sorting_Algorithms/InsertionSorter.java
@@ -4,19 +4,30 @@ public class InsertionSorter extends Sorter {
 	public InsertionSorter() {
 		type = "Insertion";
 	}
-	
+
 	// Insertion Sort is similar to Bubble Sort but a little more efficient
-	//  You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
-	void sort(int[] array, SortingVisualizer display) {		
-		//1. make a for loop that starts at 1 and goes through 
-		// the length of the array 
+	void sort(int[] arr, SortingVisualizer display) {
+		// 1. make a for loop that starts at 1 and goes through
+		// the length of the array
+		for (int i = 1; i < arr.length; i++) {
 
-			//2. make another for loop that starts at i and counts down
-        	//   while j is greater than 0
+			// 2. make another for loop that starts at i and counts down
+			// while j is greater than 0
+			for (int j = i; j > 0; j--) {
 
-				//3. if the element at j is less than the element at j - 1,
-            	//   then swap them
+				// 3. if the element at j is less than the element at j - 1,
+				// then swap them
+
+				if (arr[j] < arr[j - 1]) {
+					int arrAt0 = arr[j];
+					arr[j] = arr[(j - 1)];
+					arr[(j - 1)] = arrAt0;
+				}
+			}
+		display.updateDisplay();
+		}
 	}
 }
