d2eb20afbbbf17a11ad8030268741927dbb8635d
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-07-27 11:28:16 -0700

    quickSort

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -13,56 +13,78 @@ public class MergeSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		// 20. call the mergeSort method with 0 and the length of the array minus one
+		mergeSort(array,0,(array.length - 1),display);
 	}
 
 	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
 		// 1. Create a temporary integer array that is the same length as the passed in
 		// array.
-		int[] i = new int[array.length];
+		int[] arr = new int[array.length];
 		// 2. make an if statement that checks if low is less than high
 		// and put the rest of the method inside of it
 		if (low < high) {
 
 			// 3. Create an integer called middle and set it
 			// equal to the half way point between low and high
-			//change formulal to work with new methed
-			int middle = array[0] + array[(n - 1) - 0] / 2;
+
+			int middle = (low + high) / 2;
+
 			// 4. call the mergeSort method with low and middle
-mergeSort(array,low,middle,display);
+			mergeSort(array, low, middle, display);
+
 			// 5. call the mergeSort method with middle + 1 and high
-mergeSort(array,(middle + 1),high,display);
+			mergeSort(array, (middle + 1), high, display);
+
 			// 6. copy the elements from the array into the temporary array,
 			// but only the elements from low to high inclusive
+			for (int j = 0; j < array.length; j++) {
+				if (j <= high && j >= low) {
+					arr[j] = array[j];
+				}
+			}
 
 			// 7. create three integers called i, j, and k and
 			// set them equal to low, middle + 1, and low respectively
-
+			int i = low;
+			int j = (middle + 1);
+			int k = low;
 			// 8. while i is less than or equal to middle
 			// and j is less than or equal to high
 			// (ends after step 16)
-
-			// 9. if temp array at i is less than or equal
-			// to temp array at j
-
-			// 10. set array at k equal to temp array at i
-
-			// 11. increase i by 1
-
-			// 13. else (ends after step 15)
-
-			// 14. set array at k equal to temp array at j
-
-			// 15. increase j by 1
-
-			// 16. increase k by 1
-
+			while (i <= middle && j <= high) {
+				// 9. if temp array at i is less than or equal
+				// to temp array at j
+				if (arr[i] <= arr[j]) {
+					// 10. set array at k equal to temp array at i
+					array[k] = arr[i];
+					// 11. increase i by 1
+					i++;
+					// 13. else (ends after step 15)
+				} else {
+					// 14. set array at k equal to temp array at j
+					array[k] = arr[j];
+					// 15. increase j by 1
+					j++;
+				}
+				// 16. increase k by 1
+				k++;
+				display.updateDisplay();
+			}
 			// 17. make a while loop that runs while i is less than or equal to middle
+			while (i <= middle) {
 
-			// 18. set array at k equal to temp array at i
-
-			// 19. increase k and i by 1
-
+				// 18. set array at k equal to temp array at i
+				array[k] = arr[i];
+				// 19. inc;rease k and i by 1
+				k++;
+				i++;
+			}
 		}
+	}
+
+	private Object i(int middle) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
