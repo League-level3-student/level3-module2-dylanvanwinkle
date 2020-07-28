dd4b0f89670d45638f224245d3fc6c0fd8d94316
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-07-28 11:31:25 -0700

    bsearch

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,13 +1,13 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{	
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] arr, SortingVisualizer display) {
 
@@ -22,5 +22,5 @@ public class BubbleSorter extends Sorter{
 			display.updateDisplay();
 		}
 	}
-	
+
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -4,30 +4,59 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import _00_Sorting_Algorithms.QuickSorter;
+import _00_Sorting_Algorithms.SortingVisualizer;
+
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+
 	@Test
 	public void testLinearSearch() {
-		//1. use the assertEquals method to test your linear search method.
+		// 1. use the assertEquals method to test your linear search method.
+
+		_00_LinearSearch ls = new _00_LinearSearch();
+		String[] words = { "hi", "bye", "pizza" };
+
+		String searchValue = words[2];
+
+		int index = ls.linearSearch(words, searchValue);
+
+		assertEquals(2, index);
 	}
 
 	@Test
 	public void testBinarySearch() {
-		//2. use the assertEquals method to test your binary search method.
-		//   remember that the array must be sorted
+		// 2. use the assertEquals method to test your binary search method.
+		// remember that the array must be sorted
+		_01_BinarySearch BinarySearch = new _01_BinarySearch();
+		int[] i2 = {20000,124,132,9342};
+
+
+			for (int i = 0; i < i2.length; i++) {
+				for (int j = 0; j < i2.length - 1; j++) {
+					if (i2[j] > i2[j + 1]) {
+						int iAt0 = i2[j];
+						i2[j] = i2[(j + 1)];
+						i2[(j + 1)] = iAt0;
+				}
+				
+			}
+		}
+			int index = BinarySearch.binarySearch(i2,0,(i2.length - 1),i2[i2.length - 1]);
+			
+			assertEquals((i2.length - 1),index );
 	}
-	
+
 	@Test
 	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
+		// 3. use the assertEquals method to test your interpolation search method.
+		// remember that the array must be sorted and evenly distributed
 	}
-	
+
 	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		// 4. use the assertEquals method to test your exponential search method.
+		// remember that the array must be sorted
 	}
 }
