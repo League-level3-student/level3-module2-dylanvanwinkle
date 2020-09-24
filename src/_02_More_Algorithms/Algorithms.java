e0a9bbbc808f2f533d8c3209688c909b3db5a7ac
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-09-23 18:26:20 -0700

    algerithomsIP

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _02_More_Algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import _00_Sorting_Algorithms.SortingVisualizer;
@@ -7,9 +8,10 @@ import _01_Searching_Algorithms._00_LinearSearch;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return linearSearch(eggs,"cracked");
-		
+		return linearSearch(eggs, "cracked");
+
 	}
+
 	public static int linearSearch(List<String> eggs, String value) {
 		for (int i = 0; i < eggs.size(); i++) {
 			if (eggs.get(i).equals(value)) {
@@ -20,6 +22,7 @@ public class Algorithms {
 
 		return -1;
 	}
+
 	public static int countPearls(List<Boolean> pearls) {
 		int counter = 0;
 		for (int i = 0; i < pearls.size(); i++) {
@@ -29,82 +32,90 @@ public class Algorithms {
 		}
 		return counter;
 	}
+
 	public static double findTallest(List<Double> hights) {
-sort(hights);
-return hights.get(hights.size() - 1);
+		sort(hights);
+		return hights.get(hights.size() - 1);
 	}
+
 	static void sort(List<Double> hights) {
-		//10. call the quickSort method with 0 and the length of the array minus one
-		quickSort(hights,0,(hights.size() - 1));
+		// 10. call the quickSort method with 0 and the length of the array minus one
+		quickSort(hights, 0, (hights.size() - 1));
 	}
-	
+
 	private static void quickSort(List<Double> hights, int low, int high) {
-		//2. create two integer variables called i and j and set them equal
-		//   to low and high respectively.
-       int i = low;
-       int j = high;
-        //3. create an integer called pivot and set it equal to the element
-        //   in the array that is the halfway point between low and high
-       int middle = (low + high) / 2;
-       Double pivot = hights.get(middle);
-    		   
-        //4. make a while loop that goes while i is less than or equal to j
-        while (i <= j) {
-			
-		
-        	//5. make a while loop that increments i while 
-        	//   the array element at i is less than pivot
-            while (hights.get(i) < pivot) {
+		// 2. create two integer variables called i and j and set them equal
+		// to low and high respectively.
+		int i = low;
+		int j = high;
+		// 3. create an integer called pivot and set it equal to the element
+		// in the array that is the halfway point between low and high
+		int middle = (low + high) / 2;
+		Double pivot = hights.get(middle);
+
+		// 4. make a while loop that goes while i is less than or equal to j
+		while (i <= j) {
+
+			// 5. make a while loop that increments i while
+			// the array element at i is less than pivot
+			while (hights.get(i) < pivot) {
 				i++;
 			}
-            //6. make a while loop that decrements j while 
-        	//   the array element at j is greater than pivot
-            while(hights.get(j)> pivot) {
-            	j--;
-            }
-            //7. if i is less than or equal to j, 
-            // array elements at i and j.
-            //   In the same if-statement, increase i by 1 
-            //   and decrease j by 1.
-            if (i <= j) {
-    			Double arrayAti = hights.get(i);
-    			hights.set(i,hights.get(j)); 
-    			hights.set(j, arrayAti);
-    			i++;
-    			j--;
-    		}
-        }
-        //8. if low is less than j, call the quickSort method using
-        //   low for the low and j for the high
-        if (low < j) {
-			quickSort(hights,low,j);
+			// 6. make a while loop that decrements j while
+			// the array element at j is greater than pivot
+			while (hights.get(j) > pivot) {
+				j--;
+			}
+			// 7. if i is less than or equal to j,
+			// array elements at i and j.
+			// In the same if-statement, increase i by 1
+			// and decrease j by 1.
+			if (i <= j) {
+				Double arrayAti = hights.get(i);
+				hights.set(i, hights.get(j));
+				hights.set(j, arrayAti);
+				i++;
+				j--;
+			}
 		}
-        //9. if i is less than high, call the quickSort method using
-        //   i for the low and high for the high.
-	if (i < high) {
-		quickSort(hights,i,high);
+		// 8. if low is less than j, call the quickSort method using
+		// low for the low and j for the high
+		if (low < j) {
+			quickSort(hights, low, j);
+		}
+		// 9. if i is less than high, call the quickSort method using
+		// i for the low and high for the high.
+		if (i < high) {
+			quickSort(hights, i, high);
+		}
+
 	}
-        
-	}
+
 	public static Object findLongestWord(List<String> words) {
 		// TODO Auto-generated method stub
 		String Longest = "";
 		for (int i = 0; i < words.size(); i++) {
-			if(words.get(i).length() > Longest.length()) {
+			if (words.get(i).length() > Longest.length()) {
 				Longest = words.get(i);
 			}
 		}
 		return Longest;
 	}
+
 	public static Boolean containsSOS(List<String> message) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < message.size(); i++) {
 			if (message.get(i).contains("... --- ...")) {
-				return true;		
+				return true;
 			}
 		}
 		return false;
-		
+
 	}
 
+	public static List<Double> sortScores(List<Double> results) {
+		// TODO Auto-generated method stub
+		sort(results);
+		return results;
+	}
 }
--- src/_02_More_Algorithms/MorseCodeTranslater.java
+++ src/_02_More_Algorithms/MorseCodeTranslater.java
@@ -2,47 +2,77 @@ package _02_More_Algorithms;
 
 import java.util.HashMap;
 
+import javax.swing.JOptionPane;
+
 public class MorseCodeTranslater {
-	HashMap<String,Character> letters = new HashMap<String,Character>();
-public static void main(String[] args) {
-	new MorseCodeTranslater().Runner();
-}
-public void setUp() {
-	letters.put(".-", 'a');
-	letters.put("-...", 'b');
-	letters.put("-.-.", 'c');
-	letters.put("-..", 'd');
-	letters.put(".", 'e');
-	letters.put("..-.", 'f');
-	letters.put("--.", 'g');
-	letters.put("....", 'h');
-	letters.put("..", 'i');
-	letters.put(".---", 'j');
-	letters.put("-.-", 'k');
-	letters.put(".-..", 'l');
-	letters.put("--", 'm');
-	letters.put("-.", 'n');
-	letters.put("---", 'o');
-	letters.put(".--.", 'p');
-	letters.put("--.-", 'q');
-	letters.put(".-.", 'r');
-	letters.put("...", 's');
-	letters.put("-", 't');
-	letters.put("..-", 'u');
-	letters.put("...-", 'v');
-	letters.put(".--", 'w');
-	letters.put("-..-", 'x');
-	letters.put("-.--", 'y');
-	letters.put("--..", 'z');
-	
-}
-public void Runner() {
-	setUp();
-}
-public String translate(String morseCode) {
-	for (int i = 0; i < morseCode.length(); i++) {
-		
+	String word = "";
+	String[] letters;
+	String input;
+	HashMap<String, Character> alphebet = new HashMap<String, Character>();
+
+	public static void main(String[] args) {
+		new MorseCodeTranslater().runner();
 	}
-	return "";
-}
+
+	public void setUp() {
+		alphebet.put(".-", 'a');
+		alphebet.put("-...", 'b');
+		alphebet.put("-.-.", 'c');
+		alphebet.put("-..", 'd');
+		alphebet.put(".", 'e');
+		alphebet.put("..-.", 'f');
+		alphebet.put("--.", 'g');
+		alphebet.put("....", 'h');
+		alphebet.put("..", 'i');
+		alphebet.put(".---", 'j');
+		alphebet.put("-.-", 'k');
+		alphebet.put(".-..", 'l');
+		alphebet.put("--", 'm');
+		alphebet.put("-.", 'n');
+		alphebet.put("---", 'o');
+		alphebet.put(".--.", 'p');
+		alphebet.put("--.-", 'q');
+		alphebet.put(".-.", 'r');
+		alphebet.put("...", 's');
+		alphebet.put("-", 't');
+		alphebet.put("..-", 'u');
+		alphebet.put("...-", 'v');
+		alphebet.put(".--", 'w');
+		alphebet.put("-..-", 'x');
+		alphebet.put("-.--", 'y');
+		alphebet.put("--..", 'z');
+	}
+
+	public void runner() {
+		setUp();
+		input = JOptionPane.showInputDialog("Enter Morse Code here. put a / to indicate a new letter.");
+		solve();
+		for (int i = 0; i < letters.length; i++) {
+			word += letters[i];
+		}
+		JOptionPane.showMessageDialog(null, word);
+	}
+
+	public Character translate(String morseCode) {
+		try {
+			return alphebet.get(morseCode);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+
+	}
+
+	public void split() {
+		letters = input.split("/");
+	}
+
+	public void solve() {
+		split();
+		for (int i = 0; i < letters.length; i++) {
+			letters[i] = String.valueOf(translate(letters[i]));	
+		}
+		
+
+	}
 }
